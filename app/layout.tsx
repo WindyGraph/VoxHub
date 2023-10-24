6874cae161a2e0b0244fcd046599ec96df22df19
WindyGraph <137336036@qq.com>
2023-10-24 22:42:51 +0800

    UploadThing setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from 'next'
-import { Open_Sans } from 'next/font/google'
+// import { Open_Sans } from 'next/font/google'
+import { font } from '@/lib/font'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { cn } from '@/lib/utils'
 
-const font = Open_Sans({
-  subsets: ['latin'],
-})
+// const font = Open_Sans({
+//   subsets: ['latin'],
+// })
 
 export const metadata: Metadata = {
   title: 'Team Chat Application',
---
+++ lib/font.ts
@@ -0,0 +1,5 @@
+import { Open_Sans } from 'next/font/google'
+
+export const font = Open_Sans({
+  subsets: ['latin'],
+})
