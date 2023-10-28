18f1384bac4547212f89458a97b55f171a1dab6a
WindyGraph <137336036@qq.com>
2023-10-28 11:16:04 +0800

    Chat Input component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from 'next/navigation'
 import { currentPofile } from '@/lib/current-pofile'
 import { db } from '@/lib/db'
 import { ChatHeader } from '@/components/chat/chat-header'
+import { ChatInput } from '@/components/chat/chat-input'
 
 interface ChannelPageProps {
   params: {
@@ -42,6 +43,16 @@ const ChannelIdPage = async ({ params }: ChannelPageProps) => {
         name={channel.name}
         serverId={channel.serverId}
         type='channel'
+      />
+      <div className='flex-1'>Future Messages</div>
+      <ChatInput
+        name={channel.name}
+        type='channel'
+        apiUrl='/api/socket/messages'
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
       />
     </div>
   )
